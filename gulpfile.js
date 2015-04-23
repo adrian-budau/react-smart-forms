abb567a69e9ed5c6617509c546f076e97e42a3b5
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-04-23 03:47:53 +0300

    Clean gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,10 @@ let uglify = require('gulp-uglify');
 let watchify = require('watchify');
 let _ = require('lodash');
 
+
 gulp.task('js', function() {
-  return browserify('./example/src/app.js', { debug : false, extensions: [".jsx"] })
+  return browserify('./example/src/app.js', {debug: false,
+                                             extensions: [".jsx"]})
     .transform(babelify.configure({
       optional: ["es7.objectRestSpread"]
     }))
@@ -19,39 +21,47 @@ gulp.task('js', function() {
     .pipe(source('bundle.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(uglify({mangle:false}))
+    .pipe(uglify({mangle: false}))
     .pipe(sourcemaps.write('.'))
     .on('error', util.log.bind(util, 'Browserify Error'))
     .pipe(gulp.dest('./example/'));
 });
 
+
+// TODO: use https://www.npmjs.com/package/lazypipe for the common parts
 gulp.task('watch', function() {
-  let watch = watchify(browserify('./example/src/app.js', _.merge({debug : true}, watchify.args)));
-  watch
-    .transform(babelify.configure({
-      optional: ["es7.objectRestSpread"]
-    }));
-  let first = true;
+  let watchifyArgs = _.merge({debug : true}, watchify.args);
+  let watch = watchify(browserify('./example/src/app.js', watchifyArgs));
+
+  watch.transform(babelify.configure({
+    optional: ["es7.objectRestSpread"]
+  }));
+
+  let firstPass = true;
+
   let rebundle = function(filepath) {
     var updateStart = Date.now();
-    if (!first) {
+
+    if (!firstPass) {
       util.log('Bundling', util.colors.green(filepath) + '...');
     }
+
     return watch.bundle()
-    .pipe(source('bundle.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(uglify({mangle:false}))
-    .pipe(sourcemaps.write('.'))
-    .on('error', util.log.bind(util, 'Browserify Error'))
-    .pipe(gulp.dest('./example/'))
-    .on('end', function() {
-      if (!first) {
-        let prettyTime = (Date.now() - updateStart) + 'ms';
-        util.log('Bundled', util.colors.green(filepath), 'in', util.colors.magenta(prettyTime));
-      }
-      first = false;
-    });
+      .pipe(source('bundle.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(uglify({mangle: false}))
+      .pipe(sourcemaps.write('.'))
+      .on('error', util.log.bind(util, 'Browserify Error'))
+      .pipe(gulp.dest('./example/'))
+      .on('end', function() {
+        if (!firstPass) {
+          let prettyTime = (Date.now() - updateStart) + 'ms';
+          util.log('Bundled', util.colors.green(filepath), 'in',
+                   util.colors.magenta(prettyTime));
+        }
+        firstPass = false;
+      });
   };
 
   watch.on('update', rebundle);
